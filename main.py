e28922595db67f43368dcdd35f9dd0059ff6f6d9
Kristoffer Storhaug Pedersen <kristoffer.s.pedersen1@gmail.com>
2023-12-01 10:07:03 +0100

    update

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from flask import Flask, jsonify, request, abort, url_for, render_template, redirect, request
-from models import UserModel, TokenModel, PersonModel
-from utils import generate_token
-from functools import wraps
-from decimal import Decimal
+# Import necessary modules and packages
+from flask import Flask, jsonify, request, abort, url_for, render_template, redirect
+from models import UserModel, TokenModel, PersonModel  # Import your model classes
+from utils import generate_token  # Import a utility function
+from functools import wraps  # Import a decorator for token verification
+from decimal import Decimal  # Import Decimal for precise floating-point arithmetic
+import os  # Import the 'os' module for working with environment variables
 
-import os
-
+# Create a Flask web application
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.urandom(24)
+app.config['SECRET_KEY'] = os.urandom(24)  # Set a secret key for session security
 
 # Helper function to make person data HATEOAS compliant
 def make_public_person(person):
@@ -23,122 +24,119 @@ def make_public_person(person):
     ]
     return new_person
 
-
 # Token verification decorator
 def token_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         token = request.headers.get('Authorization')
         if not token:
-            abort(401, description="Token is missing!")
+            abort(401, description="Token is missing!")  # Unauthorized if token is missing
         if not TokenModel.verify_token(token):
-            abort(401, description="Invalid or expired token.")
+            abort(401, description="Invalid or expired token.")  # Unauthorized if token is invalid
         return f(*args, **kwargs)
     return decorated_function
 
+# Route for the home page
 @app.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def home():
-    return render_template('home.html')
+    return render_template('home.html')  # Render the home page using a template
 
-
+# Route for user registration
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
-        return render_template('register.html')
+        return render_template('register.html')  # Render the registration form
     elif request.method == 'POST':
         data = request.get_json()
         username = data.get('username')
         password = data.get('password')
 
         if not username or not password:
-            return jsonify({'message': 'Missing username or password'}), 400
+            return jsonify({'message': 'Missing username or password'}), 400  # Bad Request if missing data
 
         UserModel.create_user(username, password)
-        return redirect(url_for('login'))
+        return redirect(url_for('login'))  # Redirect to the login page after successful registration
 
-
+# Route for user login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return render_template('login.html')
+        return render_template('login.html')  # Render the login form
     elif request.method == 'POST':
         data = request.get_json()
         username = data.get('username')
         password = data.get('password')
 
         if not username or not password:
-            return jsonify({'message': 'Missing username or password'}), 400
+            return jsonify({'message': 'Missing username or password'}), 400  # Bad Request if missing data
 
         if UserModel.verify_user(username, password):
-            token = generate_token()  # Token generation no longer requires user_id
-            return jsonify({'token': token}), 200  # Return the token in JSON
+            token = generate_token()  # Generate a token for the authenticated user
+            return jsonify({'token': token}), 200  # Return the token in JSON format
 
-        return jsonify({'message': 'Invalid credentials'}), 401
+        return jsonify({'message': 'Invalid credentials'}), 401  # Unauthorized if login is unsuccessful
 
 # CRUD operations for persons
 @app.route('/persons', methods=['GET'])
-@token_required
+@token_required  # Decorate the route to require a valid token
 def get_persons():
-    persons = PersonModel.get_all_persons()
-    return jsonify([make_public_person(person) for person in persons])
+    persons = PersonModel.get_all_persons()  # Retrieve all persons from the database
+    return jsonify([make_public_person(person) for person in persons])  # Return persons in JSON format
 
 @app.route('/persons/<int:person_id>', methods=['GET'])
-@token_required
+@token_required  # Decorate the route to require a valid token
 def get_person(person_id):
-    person = PersonModel.get_person_by_id(person_id)
+    person = PersonModel.get_person_by_id(person_id)  # Retrieve a person by their ID
     if person is None:
-        abort(404)
-    return jsonify(make_public_person(person))
+        abort(404)  # Not Found if the person does not exist
+    return jsonify(make_public_person(person))  # Return the person in JSON format
 
 @app.route('/persons', methods=['POST'])
-@token_required
+@token_required  # Decorate the route to require a valid token
 def create_person():
     data = request.get_json()
     required_fields = ['id', 'name', 'address', 'phone_number', 'personnummer', 'account_balance']
 
     if not all(field in data for field in required_fields):
-        return jsonify({'message': 'Missing data'}), 400
+        return jsonify({'message': 'Missing data'}), 400  # Bad Request if missing required data
 
-    # Convert id to int and account_balance to Decimal
+    # Convert 'id' to int and 'account_balance' to Decimal
     data['id'] = int(data['id'])
     data['account_balance'] = Decimal(data['account_balance'])
 
-    PersonModel.create_person(**data)
-    return jsonify({'message': 'Person created successfully'}), 201
-
-
+    PersonModel.create_person(**data)  # Create a new person with the provided data
+    return jsonify({'message': 'Person created successfully'}), 201  # Created
 
 @app.route('/persons/<int:person_id>', methods=['PUT'])
-@token_required
+@token_required  # Decorate the route to require a valid token
 def update_person(person_id):
     data = request.get_json()
     if not data:
-        return jsonify({'message': 'No data provided'}), 400
+        return jsonify({'message': 'No data provided'}), 400  # Bad Request if no data is provided
 
     # Check if the person exists
     if not PersonModel.get_person_by_id(person_id):
-        return jsonify({'message': 'Person not found'}), 404
+        return jsonify({'message': 'Person not found'}), 404  # Not Found if the person does not exist
 
     # Exclude 'id' from the data dictionary if it's present
     data.pop('id', None)
-    # Convert account_balance to Decimal
+    # Convert 'account_balance' to Decimal
     data['account_balance'] = Decimal(data['account_balance'])
 
     # Call the update method with the converted data
     PersonModel.update_person(person_id, **data)
-    return jsonify({'message': 'Person updated successfully'}), 200
-
-
+    return jsonify({'message': 'Person updated successfully'}), 200  # OK
 
 @app.route('/persons/<int:person_id>', methods=['DELETE'])
-@token_required
+@token_required  # Decorate the route to require a valid token
 def delete_person(person_id):
     if not PersonModel.get_person_by_id(person_id):
-        return jsonify({'message': 'Person not found'}), 404
+        return jsonify({'message': 'Person not found'}), 404  # Not Found if the person does not exist
 
-    PersonModel.delete_person(person_id)
-    return jsonify({'message': 'Person deleted successfully'}), 200
+    PersonModel.delete_person(person_id)  # Delete the person from the database
+    return jsonify({'message': 'Person deleted successfully'}), 200  # OK
 
-
+# Run the Flask app if this script is the main module
 if __name__ == '__main__':
     app.run(debug=True)
+
--- models.py
+++ models.py
@@ -1,42 +1,80 @@
+# Import necessary libraries and modules
 from cassandra.cluster import Cluster
 from werkzeug.security import generate_password_hash, check_password_hash
 import hashlib
+from cassandra.auth import PlainTextAuthProvider
 import datetime
+import os
+from ssl import SSLContext, PROTOCOL_TLSv1_2, CERT_REQUIRED
+from cassandra import ConsistencyLevel
+from dotenv import load_dotenv
 
-# Establishing a connection to Cassandra
-cluster = Cluster(['127.0.0.1'])  # Add your Cassandra nodes here
-session = cluster.connect('task4')
+# Load environment variables from a .env file
+load_dotenv()
 
+# Configure SSL/TLS for secure connection
+ssl_context = SSLContext(PROTOCOL_TLSv1_2)
+ssl_context.load_verify_locations('certs/sf-class2-root.crt')  # Path to the Amazon Root CA certificate
+ssl_context.verify_mode = CERT_REQUIRED
+
+# Get credentials from environment variables
+auth_provider = PlainTextAuthProvider(
+    username=os.getenv('AMAZON_KEYSPACE_USERNAME'),
+    password=os.getenv('AMAZON_KEYSPACE_PASSWORD')
+)
+
+# Cluster configuration for Cassandra
+cluster = Cluster(
+    contact_points=['cassandra.eu-north-1.amazonaws.com'],  # Replace with your specific endpoint
+    ssl_context=ssl_context,
+    auth_provider=auth_provider,
+    port=9142
+)
+
+# Connect to the Cassandra session
+session = cluster.connect("task4")
+
+# Set default consistency level for Cassandra queries
+session.default_consistency_level = ConsistencyLevel.LOCAL_QUORUM
+
+# Define a class for managing user-related operations
 class UserModel:
     @staticmethod
     def create_user(username, password):
+        # Generate a hashed password using Werkzeug's security functions
         hashed_password = generate_password_hash(password)
+        # Create a query to insert the user data into the Cassandra 'users' table
         query = "INSERT INTO users (username, hashed_password) VALUES (%s, %s)"
         session.execute(query, (username, hashed_password))
 
     @staticmethod
     def verify_user(username, password):
+        # Query to retrieve the hashed password for a given username
         query = "SELECT hashed_password FROM users WHERE username = %s"
         user = session.execute(query, (username,)).one()
         if user:
+            # Check if the provided password matches the stored hashed password
             return check_password_hash(user.hashed_password, password)
         return False
 
+# Define a class for managing JWT token operations
 class TokenModel:
     @staticmethod
     def create_token(jwt, expiry_date):
+        # Create a query to insert a JWT token and its expiry date into the 'jwt' table
         query = "INSERT INTO jwt (jwt, expiry_date) VALUES (%s, %s)"
         session.execute(query, (jwt, expiry_date))
         
     @staticmethod
     def verify_token(auth_header):
-        # Split the header into parts and remove 'Bearer' if present
+        # Split the authorization header to extract the JWT token
         parts = auth_header.split()
         if parts[0].lower() == 'bearer' and len(parts) == 2:
             jwt = parts[1]
         else:
             jwt = auth_header.strip()  # In case the token is sent without 'Bearer'
 
+        # Query to check if the token exists and is not expired
         query = "SELECT expiry_date FROM jwt WHERE jwt = %s"
         try:
             result = session.execute(query, (jwt,)).one()
@@ -46,20 +84,22 @@ class TokenModel:
                 else:
                     print("Token expired")
             else:
-                print("Token not found in database")
+                print("Token not found in the database")
         except Exception as e:
             print(f"Error verifying token: {e}")
         return False
 
+# Define a class for managing person-related operations
 class PersonModel:
     @staticmethod
     def generate_checksum(data):
+        # Generate a checksum value based on sorted data keys
         data_string = ''.join(str(data[key]) for key in sorted(data))
         return hashlib.sha256(data_string.encode()).hexdigest()
 
     @staticmethod
     def create_person(id, name, address, phone_number, personnummer, account_balance):
-        # Generate the checksum value based on your logic
+        # Generate a checksum for the person's data
         checksum = PersonModel.generate_checksum({
             'id': id,
             'name': name,
@@ -69,7 +109,7 @@ class PersonModel:
             'account_balance': account_balance
         })
         
-        # Construct the query with all necessary fields
+        # Construct a query to insert person data into the 'persons' table
         query = """
         INSERT INTO persons (id, name, address, phone_number, personnummer, account_balance, checksum)
         VALUES (%s, %s, %s, %s, %s, %s, %s)
@@ -78,14 +118,17 @@ class PersonModel:
 
     @staticmethod
     def get_all_persons():
+        # Query to retrieve all persons from the 'persons' table
         query = "SELECT * FROM persons"
         return list(session.execute(query))
 
     @staticmethod
     def get_person_by_id(id):
+        # Query to retrieve a person by their ID
         query = "SELECT * FROM persons WHERE id = %s"
         person = session.execute(query, (id,)).one()
         if person:
+            # Verify data integrity by comparing the checksum
             person_data = {'id': person.id, 'name': person.name, 'address': person.address,
                            'phone_number': person.phone_number, 'personnummer': person.personnummer,
                            'account_balance': person.account_balance}
@@ -96,10 +139,12 @@ class PersonModel:
 
     @staticmethod
     def update_person(id, name, address, phone_number, personnummer, account_balance):
+        # Generate a checksum for the updated person data
         person_data = {'id': id, 'name': name, 'address': address,
                        'phone_number': phone_number, 'personnummer': personnummer,
                        'account_balance': account_balance}
         checksum = PersonModel.generate_checksum(person_data)
+        # Query to update person data in the 'persons' table
         query = """
         UPDATE persons SET name = %s, address = %s, phone_number = %s, 
         personnummer = %s, account_balance = %s, checksum = %s WHERE id = %s
@@ -108,5 +153,6 @@ class PersonModel:
 
     @staticmethod
     def delete_person(id):
+        # Query to delete a person by their ID
         query = "DELETE FROM persons WHERE id = %s"
         session.execute(query, (id,))
