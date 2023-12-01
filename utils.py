2b2f6bd4cab72f7acdf8b3f64e30abddceb39c42
Kristoffer Storhaug Pedersen <kristoffer.s.pedersen1@gmail.com>
2023-12-01 14:02:07 +0100

    update

--- utils.py
+++ utils.py
@@ -4,6 +4,6 @@ from models import TokenModel
 
 def generate_token():
     jwt = str(uuid.uuid4())  # Generate a unique token
-    expiry_date = datetime.utcnow() + timedelta(days=2)  # Set token expiry to 10 days from now
+    expiry_date = datetime.utcnow() + timedelta(days=2)  # Set token expiry to 2 days from now
     TokenModel.create_token(jwt, expiry_date)
     return jwt
